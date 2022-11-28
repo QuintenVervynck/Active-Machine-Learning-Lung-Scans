b19774a93b94be0d6cb2db73eb0f03d35de2b29c
quintenvervynck <vervynck.quinten@gmail.com>
2022-11-28 21:28:24 +0100

    full learn

--- notebooks/full_learn_acc_graph.py
+++ notebooks/full_learn_acc_graph.py
@@ -3,24 +3,40 @@ import matplotlib.pyplot as plt
 from lib.dataset import Dataset
 from lib.model import Model
 
-if __name__ == "__main__":    
-    print("FULL LEARN ACC GRAPH")
-    dataset = Dataset()
-        
-    steps = [i for i in range(50, 1050, 50)] + [i for i in range(1100, 3201, 100)]
-    models = []
+if __name__ == "__main__":
+    mode = "load"  # load or calculate
 
-    for i, max_imgs in enumerate(steps):
-        print(f"TRAINING MODEL {i+1}/{len(steps)} ON {max_imgs} IMGS")
-        models.append(
-            Model("vgg16", dataset).limit_training_data(max_imgs).train().test().clean_full()
-        )
-        with open("full_learn_acc_graph_checkpoints.txt", "a+") as f:
-            f.write(f"{str(models[-1])}\n")
+    if mode == "calculate":
+        print("FULL LEARN ACC GRAPH")
+        dataset = Dataset()
+
+        steps = [i for i in range(50, 1050, 50)] + [i for i in range(1100, 3201, 100)]
+        models = []
+
+        for i, max_imgs in enumerate(steps):
+            print(f"TRAINING MODEL {i+1}/{len(steps)} ON {max_imgs} IMGS")
+            models.append(
+                Model("vgg16", dataset).limit_training_data(max_imgs).train().test().clean_full()
+            )
+            with open("full_learn_acc_graph_checkpoints.txt", "a+") as f:
+                f.write(f"{str(models[-1])}\n")
+            plt.suptitle("Accuracy graph (VGG16)")
+            plt.xlabel("Used samples")
+            plt.ylabel("Accuracy")
+            plt.plot([sum(m.used) for m in models], [m.acc for m in models])
+            plt.savefig("full_learn_acc_graph.png")
+
+    elif mode == "load":
+        datafile = "results/full_learn/full_learn_acc_graph_checkpoints.txt"
+        used = []
+        accs = []
+        with open (datafile, "r") as f:
+            while(line:=f.readline()):
+                line = line.split()
+                used.append(int(line[3]))
+                accs.append(float(line[-1][:-1]))        
         plt.suptitle("Accuracy graph (VGG16)")
         plt.xlabel("Used samples")
         plt.ylabel("Accuracy")
-        plt.plot([sum(m.used) for m in models], [m.acc for m in models])
-        plt.savefig("full_learn_acc_graph.png")
-
-    
+        plt.plot(used, accs)
+        plt.show()
