13dacc7319ee95abd0592839195fdac61c2d14cb
KoenDesplenter <koen.desplenter@telenet.be>
2022-12-01 21:25:33 +0100

    process enhance images

--- notebooks/lib/dataset.py
+++ notebooks/lib/dataset.py
@@ -1,3 +1,4 @@
+import multiprocessing as mlp
 import os
 import zipfile
 import matplotlib.pyplot as plt
@@ -23,22 +24,30 @@ class Dataset():
         self.revnames = {"COVID": 0, "Normal": 1, "Viral Pneumonia": 2}
         self.X = None
         self.y = None
-        
+
+        # Dataset folder doesn't exist, download files, resize to 1345 per class, downsize images to 244x244x3
         if not os.path.isdir(self.path):
             self.download()   
             self.load_local()
             self.restrict_size_per_class()
+            self.convert_size()
         
-
+        # Use enhance dataset
         if enhance_data:
+            # X and y numpy arrays don't exist
             if not os.path.isfile(f"{self.enhanced}/x.npy") or not os.path.isfile(f"{self.enhanced}/y.npy"):
                 print(f"Enhancing dataset")
+
+                # Data paths are not loaded
                 if not len(self.data):
                     self.load_local()
-                self.enhance_images()
-                self.load_local(f"{self.enhanced}/COVID-19_Radiography_Dataset/")
-                self.prepare_images()
-                self.save(path=self.enhanced)
+                
+                # # Enhance the images
+                # self.enhance_images()
+                # #Load new data paths
+                # self.load_local(f"{self.enhanced}/COVID-19_Radiography_Dataset/")
+                # self.prepare_images()
+                # self.save(path=self.enhanced)
             else:
                 self.load(path=self.enhanced)
                 self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
@@ -49,8 +58,11 @@ class Dataset():
                     random_state=42
                 )
                 self.size = self.X_train.shape[0]
+        # Use normal dataset
         else:
+            # X and y numpy arrays don't exist
             if not os.path.isfile(f"{self.path}/x.npy") or not os.path.isfile(f"{self.path}/y.npy"):
+                # Data paths are not loaded
                 if not len(self.data):
                     self.load_local()
                 self.prepare_images()
@@ -68,21 +80,8 @@ class Dataset():
             
         print("Finished")
             
-    def to_label(self, x:int):
-        return self.names[x]
-    
-    
-    def from_label(self, x:str):
-        return self.revnames[x]
-    
-    def to_classlist(self, x:int):
-        l = [0.0, 0.0, 0.0]
-        l[x] = 1.0
-        return l
-    
-    
-    def from_classlist(self, x):
-        return np.argmax(x)
+    ##### Functions used in downloading #####
+
     def download(self):                    
         if not os.path.isfile(f"{self.path}/covid19-radiography-database.zip"):
             print("Downloading...")
@@ -110,6 +109,152 @@ class Dataset():
             self.size += len(self.data[subfolder + 'images'])
         print(f"  Total size: {str(self.size)}")
     
+    def restrict_size_per_class(self, size=1345):
+        """
+        Downsize number of images to size
+        - sort images numerically
+        - try to remove image
+        Only used in download part
+        """
+        # Remove images larger than 1345
+        print(f"Removing all images greater than {self.class_length}")
+        for subfolder in self.subfolders:
+            for version in self.versions:
+                file_list = self.data[subfolder + version]
+                file_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+                failed = []
+                for file_path in file_list[self.class_length:]:
+                    try:
+                        os.remove(file_path)
+                    except:
+                        failed.append(file_path)
+                        print("Error while deleting file : ", file_path)
+                self.data[subfolder + version] = file_list[:self.class_length] + failed
+    
+    def convert_size(self):
+        """
+        Adjust the images:
+        - resize to 224x224
+        - convert greyscale to RGB
+        Only used in download part
+        """
+        print(f"Converting size of the images to 244x244x3")
+        for subfolder in self.subfolders:
+            for i, image_path in enumerate(self.data[subfolder + "images"]):
+                image = plt.imread(image_path)
+
+                # Some images are not 8 bit, so we convert them
+                if (len(image.shape) != 2):
+                    img = Image.open(image_path)
+                    new_img = img.convert("L")
+                    new_img.save(image_path)
+
+                # Convert image to 224,224,3
+                img = Image.open(image_path)
+                new_img = img.convert("RGB")
+                new_img = new_img.resize(size=(224, 224))
+                new_img.save(image_path)
+
+                # Convert mask to 224,224,3
+                mask = self.data[subfolder + "masks"][i]
+                msk = Image.open(mask)
+                new_msk = msk.resize(size=(224, 224))
+                new_msk.save(mask)
+
+    ##### Functions used in dataset preparation #####
+    
+    def prepare_images(self):
+        """
+        Create the training data:
+        - apply the mask on the image
+        """
+        print("Preprocessing the images...")
+        X = np.zeros((self.size, 224, 224, 3), dtype=np.uint8)
+        index = 0
+        test = 0
+        for subfolder in self.subfolders:
+            for i, image_path in enumerate(self.data[subfolder + "images"]):
+                img = Image.open(image_path)
+                np_image = np.array(img)
+
+                mask = self.data[subfolder + "masks"][i]
+                msk = Image.open(mask)
+                np_mask = np.array(msk)
+
+                # Apply the mask
+                new_image = np.multiply(np_image, np_mask)
+
+                X[index] = new_image
+                index += 1
+
+        y = np.concatenate([np.array([subfolder[:-1]] * len(self.data[subfolder + "images"])) for subfolder in self.subfolders])
+        y = np.array(list(map(self.from_label, y))) # map labels to ints of labels
+        print(f"  size of training data: {len(X)}")
+        print(f"  size of training data labels: {len(y)}")
+        self.X = X
+        self.y = y
+        return (X,y)
+
+    def enhance_images(self):
+        processes = []
+        cores = (mlp.cpu_count() - 2) // 3
+        print(f"Using {cores * 3} cores to enhance image")
+
+        for subfolder in self.subfolders:
+            print(f"Enhancing: {self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images")
+            os.makedirs(f"{self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images", exist_ok=True)
+
+            for files in np.array_split(self.data[subfolder + "images"], cores):
+                p = mlp.Process(target=self.enhance, args=(files, subfolder))
+                p.start()
+        
+        print(f"Made a total of {len(processes)} to enhance images")
+        for process in processes:
+            process.join()
+
+
+    def enhance(self, files, subfolder):
+        for file_path in files:
+            alg = CLAHE(file_path, f"{self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images", 100, 150, 1)
+            alg.run() 
+
+
+    ##### Other functions #####   
+              
+    def save(self, path=""):
+        path = self.path if not path else path
+        print(f"Saving the preprocessed dataset...")
+        np.save(f"{path}/x", self.X)
+        np.save(f"{path}/y", self.y)
+              
+    def load(self, path=""):
+        path = self.path if not path else path
+        print(f"Loading local preprocessed dataset...")
+        self.X = np.load(f"{path}/x.npy")
+        self.y = np.load(f"{path}/y.npy")
+    
+    def split(self):
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
+    def to_label(self, x:int):
+        return self.names[x]
+    
+    def from_label(self, x:str):
+        return self.revnames[x]
+    
+    def to_classlist(self, x:int):
+        l = [0.0, 0.0, 0.0]
+        l[x] = 1.0
+        return l
+    
+    def from_classlist(self, x):
+        return np.argmax(x)
+
+    def show_processed_example(self):
+        print(f"Example image with label {self.to_label(self.y[0])}:")
+        plt.imshow(self.X[0])
+
     def show_unprocessed_examples(self):
         if "COVID/images" not in self.data:
             self.load_local()
@@ -127,106 +272,3 @@ class Dataset():
             ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
             ax.set_title("%s" % label.split("/")[-1])
     
-    def restrict_size_per_class(self, size=1345):
-        # Remove images larger than 1345
-        print(f"Removing all images greater than {self.class_length}")
-        for subfolder in self.subfolders:
-            for version in self.versions:
-                file_list = self.data[subfolder + version]
-                file_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-
-                failed = []
-                for file_path in file_list[self.class_length:]:
-                    try:
-                        os.remove(file_path)
-                    except:
-                        failed.append(file_path)
-                        print("Error while deleting file : ", file_path)
-                self.data[subfolder + version] = file_list[:self.class_length] + failed    
-    
-    def prepare_images(self):
-        """
-        Create the training data:
-        - resize to 224x224
-        - convert greyscale to RGB
-        - apply the mask on the image
-        """
-        print("Preprocessing the images...")
-        X = np.zeros((self.size, 224, 224, 3), dtype=np.uint8)
-        index = 0
-        test = 0
-        for subfolder in self.subfolders:
-            for i, image_path in enumerate(self.data[subfolder + "images"]):
-                image = plt.imread(image_path)
-
-                # Some images are not 8 bit, so we convert them
-                if (len(image.shape) != 2):
-                    img = Image.open(image_path)
-                    new_img = img.convert("L")
-                    new_img.save(image_path)
-
-                # Convert image to 224,224,3
-                img = Image.open(image_path)
-                new_img = img.convert("RGB")
-                new_img = new_img.resize(size=(224, 224))
-                np_image = np.array(new_img)
-
-                mask = self.data[subfolder + "masks"][i]
-                # Convert mask to 224,224,3
-                msk = Image.open(mask)
-                new_msk = msk.resize(size=(224, 224))
-                np_mask = np.array(new_msk)
-
-                # Apply the mask
-                new_image = np.multiply(np_image, np_mask)
-
-                X[index] = new_image
-                index += 1
-
-        y = np.concatenate([np.array([subfolder[:-1]] * len(self.data[subfolder + "images"])) for subfolder in self.subfolders])
-        y = np.array(list(map(self.from_label, y))) # map labels to ints of labels
-        print(f"  size of training data: {len(X)}")
-        print(f"  size of training data labels: {len(y)}")
-        self.X = X
-        self.y = y
-        return (X,y)
-
-    def enhance_images(self):
-        for subfolder in self.subfolders:
-            print(f"Enhancing: {self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images")
-            os.makedirs(f"{self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images", exist_ok=True)
-
-            for file_path in self.data[subfolder + "images"]:
-                
-                alg = CLAHE(file_path, f"{self.enhanced}/COVID-19_Radiography_Dataset/{subfolder}images", 100, 150, 1)
-                alg.run()
-
-    def enhance(self):
-        if not os.path.isdir(self.enhanced):
-            print(f"Enhancing dataset")
-            if not len(self.data):
-                self.load_local()
-            self.enhance()
-            self.load_local(f"{self.enhanced}/COVID-19_Radiography_Dataset/")
-            self.prepare_images()
-            self.save()
-    
-    def show_processed_example(self):
-        print(f"Example image with label {self.to_label(self.y[0])}:")
-        plt.imshow(self.X[0])
-    
-              
-    def save(self, path=""):
-        path = self.path if not path else path
-        print(f"Saving the preprocessed dataset...")
-        np.save(f"{path}/x", self.X)
-        np.save(f"{path}/y", self.y)
-              
-    def load(self, path=""):
-        path = self.path if not path else path
-        print(f"Loading local preprocessed dataset...")
-        self.X = np.load(f"{path}/x.npy")
-        self.y = np.load(f"{path}/y.npy")
-    
-    def split(self):
-        return self.X_train, self.X_test, self.y_train, self.y_test
